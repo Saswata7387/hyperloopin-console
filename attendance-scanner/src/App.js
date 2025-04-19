8bc482003c75dacf3113888964dc46e4fec4ac63
Naveen Biju <naveenbiju2005@gmail.com>
2025-04-19 20:56:27 +0530

    attendance scanner

---
+++ attendance-scanner/src/App.js
@@ -0,0 +1,9 @@
+import logo from './logo.svg';
+import './App.css';
+import AttendanceScanner from './AttendanceScanner';
+
+function App() {      
+    return <AttendanceScanner />;
+}
+
+export default App;
