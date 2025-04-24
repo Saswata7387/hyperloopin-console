05add1b44f63b8ae68de62caf6a0b795dc80ccce
Lightningbolt935 <monster92171@gmail.com>
2025-04-24 10:17:27 +0530

    Update InputField.jsx

--- Frontend-LoginPage/src/components/InputField.jsx
+++ Frontend-LoginPage/src/components/InputField.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEnvelope, faLock } from "@fortawesome/free-solid-svg-icons";
 
-const InputField = ({ type, placeholder, icon }) => {
+const InputField = ({ type, placeholder, icon ,value, onChange}) => {
   const getIcon = () => {
     if (icon === "mail") return faEnvelope;
     if (icon === "lock") return faLock;
@@ -12,7 +12,12 @@ const InputField = ({ type, placeholder, icon }) => {
   return (
     <div className="input-field">
       <FontAwesomeIcon icon={getIcon()} className="input-icon" />
-      <input type={type} placeholder={placeholder} />
+      <input
+        type={type}
+        placeholder={placeholder}
+        value={value}
+        onChange={onChange}
+      />
     </div>
   );
 };
